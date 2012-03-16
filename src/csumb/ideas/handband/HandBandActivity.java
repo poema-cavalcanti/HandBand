031b4952fe687baa8a82b92e7194d59d3198a9d6
Poema Cavalcanti <poema.campos@gmail.com>
2012-03-16 15:51:09 -0700

    movement using touch can now be tracked

--- src/csumb/ideas/handband/HandBandActivity.java
+++ src/csumb/ideas/handband/HandBandActivity.java
@@ -2,12 +2,52 @@ package csumb.ideas.handband;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.util.*;
 
-public class HandBandActivity extends Activity {
+public class HandBandActivity extends Activity implements OnTouchListener{
     /** Called when the activity is first created. */
+	StringBuilder builder = new StringBuilder();
+	TextView textview;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		textview = new TextView(this);
+		textview.setText("Touch anywhere!"); 
+		textview.setOnTouchListener(this); //registers our activity to with the textview so it recieves MotionEvents
+		setContentView(textview);
     }
+	
+	public boolean onTouch(View v, MotionEvent event) {
+		builder.setLength(0);
+		switch (event.getAction()){
+		case MotionEvent.ACTION_DOWN:
+			builder.append("down, ");
+			break;
+		case MotionEvent.ACTION_MOVE:
+			builder.append("move, ");
+			break;
+		case MotionEvent.ACTION_CANCEL:
+			builder.append("cancel, ");
+			break;
+		case MotionEvent.ACTION_UP:
+			builder.append("up, ");
+			break;
+		}
+		builder.append(event.getX());
+		builder.append(", ");
+		builder.append(event.getY());
+		String text = builder.toString();
+		Log.d("Touch Test", text);
+		textview.setText(text);
+		
+		return true;
+	}
 }
