0d70d12eb59eb7cd20d73276633e3c31fc34cb8a
Poema Cavalcanti <poema.campos@gmail.com>
2012-03-17 14:13:56 -0700

    Juan motion sound integration

--- gen/csumb/ideas/handband/R.java
+++ gen/csumb/ideas/handband/R.java
@@ -18,8 +18,15 @@ public final class R {
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class raw {
+        public static final int bongo1=0x7f040000;
+        public static final int bongo2=0x7f040001;
+        public static final int bongo3=0x7f040002;
+        public static final int bongo4=0x7f040003;
+        public static final int triangle=0x7f040004;
+    }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
 }
--- src/csumb/ideas/handband/HandBandActivity.java
+++ src/csumb/ideas/handband/HandBandActivity.java
@@ -12,18 +12,31 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.util.*;
+import csumb.ideas.handband.Sounds;
 
 public class HandBandActivity extends Activity implements OnTouchListener{
     /** Called when the activity is first created. */
 	StringBuilder builder = new StringBuilder();
 	TextView textview;
+	private Sounds mSoundManager;
+    private Sounds triangle;
 
 	//onCreate method sets the window to fullscreen and creates an onTouchListener
     @Override
 	public void onCreate(Bundle savedInstanceState) {
     	requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
 		super.onCreate(savedInstanceState);
+        
+		mSoundManager = new Sounds();
+        mSoundManager.initSounds(getBaseContext());
+        mSoundManager.addSound(1, R.raw.bongo1);
+
+        triangle = new Sounds();
+        triangle.initSounds(getBaseContext());
+        triangle.addSound(1, R.raw.triangle);
+		
 		textview = new TextView(this);
 		textview.setText("Touch anywhere!"); 
 		textview.setOnTouchListener(this); //registers our activity to with the textview so it recieves MotionEvents
@@ -55,6 +68,16 @@ public class HandBandActivity extends Activity implements OnTouchListener{
 		Log.d("Touch Test", text);
 		textview.setText(text);
 		
+		//Play Sounds
+    	if(event.getX() < 160 && event.getY() < 240)
+    	{
+    		mSoundManager.playSound(1);
+    	}
+    	if(event.getX() > 160 && event.getY() < 240)
+    	{
+    		triangle.playSound(1);
+    	}
+    	
 		return true;
 	}
 }
